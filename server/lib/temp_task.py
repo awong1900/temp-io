cdd54df2eb09deee38d922f86b190a2d9e7fd879
Ten Wong <wangtengoo7@gmail.com>
2016-12-08 16:19:32 +0800

    PEP8

--- server/lib/temp_task.py
+++ server/lib/temp_task.py
@@ -13,8 +13,8 @@ from db import Temp
 token = '88f9727e6284476b6de1122cf9c0d1d7'
 test_url = 'https://us.wio.seeed.io/v1/node/GroveTempHumD0/temperature?access_token={}'.format(token)
 sleep_url = 'https://us.wio.seeed.io/v1/node/pm/sleep'
-# [sleep_time_sec]?
 tasks = set()
+
 
 @gen.coroutine
 def task(*args):
@@ -33,7 +33,8 @@ def task(*args):
     
     if not is_open:
         gen_log.info("Temp({}) be closed!".format(args[0]))
-        yield Temp().update_temp(temp_id,
+        yield Temp().update_temp(
+            temp_id,
             {"status": "normal", "status_text": "The temp is closed."})
         raise gen.Return()
         
@@ -50,16 +51,18 @@ def task(*args):
             print temps
             temp = round(sum(temps[1:])/(len(temps)-1), 1) 
         except Exception as e:
-            if (time.time() > end_time):
+            if time.time() > end_time:
                 gen_log.error(e)
-                yield Temp().update_temp(temp_id,
+                yield Temp().update_temp(
+                    temp_id,
                     {"status": "error", "status_text": "The node is not wake up on three period.", "open": False})
                 raise gen.Return()
             yield gen.sleep(5)
             gen_log.info(e)
             continue
             
-        yield Temp().update_temp(temp_id,
+        yield Temp().update_temp(
+            temp_id,
             {"temperature": temp, "temperature_at": datetime.utcnow()},
             {"value": temp, "created_at": datetime.utcnow()})
         print temp
@@ -69,8 +72,10 @@ def task(*args):
     if has_sleep is True:
         try:
             body = urllib.urlencode({})
-            res = yield http_client.fetch("{}/{}?access_token={}".format(sleep_url, period-10, token), method='POST', body=body)
-            yield Temp().update_temp(temp_id,
+            res = yield http_client.fetch("{}/{}?access_token={}".format(sleep_url, period-10, token), method='POST',
+                                          body=body)
+            yield Temp().update_temp(
+                temp_id,
                 {"status": "normal", "status_text": "The node is on sleep mode."})
             print res.body
         except Exception as e:
@@ -80,6 +85,7 @@ def task(*args):
     IOLoop.current().add_timeout(time.time() + (period or 5), task, temp_id)
     print tasks
     gen_log.info("===End task and restart({})====".format(args[0]))
+
 
 def is_task_queue(temp_id):
     return True if temp_id in tasks else False
